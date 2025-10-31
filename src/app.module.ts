6bfb21f7eb08582dbea44d93c8098253a3ebebec
jes11sy <defouz03@gmail.com>
2025-10-31 13:26:10 +0400

    logs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 import { AuthModule } from './auth/auth.module';
 import { FilesModule } from './files/files.module';
 
@@ -18,6 +19,10 @@ import { FilesModule } from './files/files.module';
         limit: 100, // 100 запросов по умолчанию
       },
     ]),
+    PrometheusModule.register({
+      defaultMetrics: { enabled: true },
+      path: '/metrics',
+    }),
     AuthModule,
     FilesModule,
   ],
